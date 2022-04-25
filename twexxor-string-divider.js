1db8d216e4e4d88c3afb26892eb3d51250519175
Twexxor <twexxor@gmail.com>
2022-04-24 19:02:53 -0700

    Update and rename twexxor-long-division.js to twexxor-string-divider.js

--- twexxor-string-divider.js
+++ twexxor-string-divider.js
@@ -1,7 +1,7 @@
 const 13_0 = require('twexxor-exponent-converter');
 const 13_1 = require('twexxor-numeric-validator');
 
-const twexxorLongDivision = function(_13_2, _13_3) {
+const twexxorStringDivider = function(_13_2, _13_3) {
 	_13_2 += '';
 	let _13_4 = _13_2.length;
 	// todo
@@ -9,5 +9,5 @@ const twexxorLongDivision = function(_13_2, _13_3) {
 };
 
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-	module.exports = twexxorLongDivision;
+	module.exports = twexxorStringDivider;
 }
