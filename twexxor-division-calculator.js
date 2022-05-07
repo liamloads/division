67c127fc51d0d57099da373b62b71b37785df7c3
Will Parsons <twexxor@gmail.com>
2022-05-06 19:47:07 -0700

    Update twexxor-division-calculator.js

--- twexxor-division-calculator.js
+++ twexxor-division-calculator.js
@@ -1,7 +1,24 @@
 const _16_0 = {'NaN': 1};
 const _16_1 = require('twexxor-addition-calculator');
+const _16_2 = require('twexxor-highest-detector');
 
-const twexxorDivisionCalculator = function(_16_2, _16_3) {
+const twexxorDivisionCalculator = function(_16_3, _16_4) {
+	_16_3 = [
+		_16_3,
+		_16_4
+	];
+	_16_4 = 0;
+	let _16_5 = 1;
+
+	while (_16_4 !== 2) {
+		_16_3[_16_4] += '';
+
+		if (_16_3[_16_4++][0] !== '-') {
+			_16_3[_16_4 - 1] = '-' + _16_3[_16_4 - 1];
+			_16_5++;
+		}
+	}
+
 	// todo
 };
 
