9a0f42ee95e2c3a7a4a60d35c3f450f85eca1baf
Will Parsons <twexxor@gmail.com>
2022-05-06 17:13:08 -0700

    Create twexxor-division-calculator.js

---
+++ twexxor-division-calculator.js
@@ -0,0 +1,11 @@
+const _16_0 = {'NaN': 1};
+const _16_1 = require('twexxor-addition-calculator');
+const _16_2 = require('twexxor-exponent-converter');
+
+const twexxorDivisionCalculator = function(_16_3, _16_4) {
+	// todo
+};
+
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+	module.exports = twexxorDivisionCalculator;
+}
