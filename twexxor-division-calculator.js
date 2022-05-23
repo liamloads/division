74210ad71457b157b3933f806d20e79b7befc60a
Will Parsons <twexxor@gmail.com>
2022-05-23 16:20:53 -0700

    Update twexxor-division-calculator.js

--- twexxor-division-calculator.js
+++ twexxor-division-calculator.js
@@ -3,24 +3,22 @@ const _16_1 = require('twexxor-quotient-calculator');
 const _16_2 = require('twexxor-string-repeater');
 
 const twexxorDivisionCalculator = function(_16_3, _16_4, _16_5) {
-	_16_3 = [
-		_16_0(_16_3, '0'),
-		_16_0(_16_4, '0')
-	];
+	_16_3 = _16_0(_16_3, '0');
+	_16_4 = _16_0(_16_4, '0');
 
-	if (_16_3[0] === false || (_16_3[1] - 0)  === 0) {
+	if (_16_3 === false || (_16_4 - 0)  === 0) {
 		return false;
 	}
 
-	if (_16_3[0] === '0') {
+	if (_16_3 === '0') {
 		return '0';
 	}
 
-	if (_16_3[1] === '1') {
+	if (_16_4 === '1') {
 		return _16_3[0];
 	}
 
-	if (_16_3[0] === _16_3[1]) {
+	if (_16_3 === _16_4) {
 		return '1';
 	}
 
@@ -30,9 +28,15 @@ const twexxorDivisionCalculator = function(_16_3, _16_4, _16_5) {
 		_16_5 = 100;
 	}
 
-	_16_4 = '';
+	let _16_6 = _16_1(_16_3, _16_4);
+	let _16_7 = _16_1(_16_3 + '0', _16_4);
+
+	if (_16_7[1] = '0') {
+		return _16_6;
+	}
+
 	// todo
-	return _16_4;
+	return _16_6;
 };
 
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
